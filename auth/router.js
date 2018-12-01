cf5293aa8bc6af8950550efc7446d2c2b1e7f147
Peeter Mäeots <maeots.peeter@gmail.com>
2018-12-01 22:12:23 +0200

    auth refresh fixed

--- auth/router.js
+++ auth/router.js
@@ -32,7 +32,7 @@ const jwtAuth = passport.authenticate('jwt', {session: false});
 // The user exchanges a valid JWT for a new one with a later expiration
 router.post('/refresh', jwtAuth, (req, res) => {
   const authToken = createAuthToken(req.user);
-  res.json({authToken, YUMMLY_ID, YUMMLY_KEY});
+  res.json({authToken});
 });
 
 module.exports = {router};
